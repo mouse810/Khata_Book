18be71828fa510da3d91733af7dccf28debb5563
Chetan Chouhan <mousegamingv2.0@gmail.com>
2022-04-07 15:09:45 +0530

    1st Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import Expenses from "./components/Expenses";
+import Expenses from "./components/Expenses/Expenses";
 function App() {
   const expenses = [
     {
@@ -14,29 +14,34 @@ function App() {
       title: "Dor Lock",
       amount: 346.53,
       date: new Date(2022, 2, 23),
-
     },
     {
       id: "e3",
       title: "toilet cleaner",
       amount: 256.83,
       date: new Date(2022, 2, 18),
-
     },
     {
       id: "e4",
       title: "toilet brushs",
       amount: 238.03,
       date: new Date(2022, 2, 15),
-
     },
   ];
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses}/>
-    </div>
+
+  return React.createElement(
+    "div",
+    {},
+    React.createElement("h2", {}, "Lets Started!"),
+    React.createElement(Expenses, { items: expenses })
   );
+
+  // return (
+  //   <div>
+  //     <h2>Let's get started!</h2>
+  //     <Expenses items={expenses}/>
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Card from './Card';
+import React from "react";
+import Card from '../UI/Card';
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 function Expenses(props) {
