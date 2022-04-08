d9ea3da76ea5a6ded13b2d129ec81e7e380aa8cf
Chetan Chouhan <mousegamingv2.0@gmail.com>
2022-04-08 10:51:32 +0530

    2st Commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
-function App() {
+const App=() => {
   const expenses = [
     {
       id: "e1",
@@ -32,7 +32,7 @@ function App() {
   return React.createElement(
     "div",
     {},
-    React.createElement("h2", {}, "Lets Started!"),
+    React.createElement("h2", {}, "Lets Get Started!"),
     React.createElement(Expenses, { items: expenses })
   );
 
